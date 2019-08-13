4e204c36cad7d217756bc3002baeb36f3910c5e1
jorge <c0de42.com@gmail.com>
2019-08-13 10:01:57 +0200

    Versión Inicial

---
+++ src/test/java/es/alwaysnear/eureka/AlwaysnearEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package es.alwaysnear.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AlwaysnearEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
